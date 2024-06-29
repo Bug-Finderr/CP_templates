e4367b5d3ae5aadee18597e49d7ab021c3505537
Bug-Finderr <sudu.boi.2k5@gmail.com>
2024-06-29 17:31:37 +0530

    Basic V0

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Aliases
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+// Constants
+constexpr int INF = 1e9;
+constexpr int MOD = 1e9 + 7;
+
+// Macros
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+
+// Function prototypes
+void solve();
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    
+    // cin >> t;       // Uncomment the following line if there are multiple test cases
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
+void solve() {
+    // Read input
+    // Example: int n; cin >> n;
+    
+    // Process input and solve the problem
+    // Example: cout << n * n << endl;
+
+    // Output result
+}
