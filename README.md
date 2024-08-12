9b5ecfa9483db1730dd554e52167c4912129303d
Bug-Finderr <sudu.boi.2k5@gmail.com>
2024-08-13 01:05:55 +0530

    Segment Tree snippet

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CP_templates
+
 Templates for competitive programming written in C++
+
+The cpp.json contains my user snippeets for VSCode
+Import them and use shortcuts to import desired snippet into your code
--- main.cpp
+++ main.cpp
@@ -1,43 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Aliases
-using ll = long long;
-using ld = long double;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
-using vi = vector<int>;
-using vll = vector<ll>;
-
-// Constants
-constexpr int INF = 1e9;
-constexpr int MOD = 1e9 + 7;
-
 // Macros
-#define F first
-#define S second
-#define PB push_back
-#define MP make_pair
 #define ALL(x) (x).begin(), (x).end()
 #define RALL(x) (x).rbegin(), (x).rend()
+#define int long long           // Comment this line if you want to use actual int
+#define double long double      // Comment this line if you want to use actual double
 
-// Function prototypes
-void solve();
+// Aliases
+using ull = unsigned long long;
+using p = pair<int, int>;
+using v = vector<int>;
+using m = map<int, int>;
+using vv = vector<v>;
+using us = unordered_set<int>;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+// Constants
+constexpr int inf = LLONG_MAX;
+constexpr int mod = 1e9 + 7;
 
-    int t = 1;
-    
-    // cin >> t;       // Uncomment the following line if there are multiple test cases
 
-    while (t--) {
-        solve();
-    }
-
-    return 0;
-}
 
 void solve() {
     // Read input
@@ -47,4 +29,20 @@ void solve() {
     // Example: cout << n * n << endl;
 
     // Output result
+}
+
+
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    
+    // cin >> t;       // Uncomment the following line if there are multiple test cases
+
+    while (t--)
+        solve();
+
+    return 0;
 }
